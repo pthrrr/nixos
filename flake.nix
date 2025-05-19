406bcd5f84276e923d1cc8e63f6f12bf67638b1a
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-05-19 11:46:45 +0200

    setup home manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Add Home Manager as an input
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      ...
-    }@inputs:
+    { self, nixpkgs, home-manager, ... }@inputs:
 
     let
       # System types to support.
@@ -34,17 +36,37 @@
 
       nixosConfigurations = {
         laptop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+          #system = "x86_64-linux";
+          
           modules = [
             self.nixosModules.common
             ./hosts/laptop
+            
+            # Add Home Manager as a module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              # Point to your home configurations
+              home-manager.users.pthr = import ./modules/home.nix;
+            }
           ];
         };
+
         desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             self.nixosModules.common
             ./hosts/desktop
+
+            # Add Home Manager as a module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              # Point to your home configurations
+              home-manager.users.pthr = import ./modules/home.nix;
+            }
           ];
         };
       };
---
+++ modules/home.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+
+{
+
+  #imports = [ ./../hosts/desktop/default.nix ];
+
+  # Home Manager settings
+  home.username = "pthr";
+  home.homeDirectory = "/home/pthr";
+  home.stateVersion = "25.05";
+
+  # Let Home Manager manage itself
+  programs.home-manager.enable = true;
+
+  # Packages specific to your user
+  home.packages = with pkgs; [ ];
+
+  # Program-specific configurations
+  #programs.git = {
+  #  enable = true;
+  #  #userName = "Your Name";
+  #  #userEmail = "your.email@example.com";
+  #};
+
+  # Additional configurations...
+}
