6de7cbd31973d696cd82f98cad04c50c3ab168ef
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-05-28 13:27:30 +0200

    add comment

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     ./applications.nix
-    ./gaming.nix
+    # ./../gaming.nix         is host specifig, imported in hosts/desktop,laptop/configuration.nix
     ./house-keeping.nix
     ./samba.nix
     ./users.nix
