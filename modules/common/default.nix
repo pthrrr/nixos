e252c26b6fde00528288e32b2396dd432cf657d7
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-05-29 10:14:18 +0200

    disable mouse acceleration

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -3,6 +3,7 @@
     ./applications.nix
     # ./../gaming.nix         is host specifig, imported in hosts/desktop,laptop/configuration.nix
     ./house-keeping.nix
+    ./mouse.nix
     ./samba.nix
     ./users.nix
   ];
