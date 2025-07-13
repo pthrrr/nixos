1354f295eee1fbebe3c2be38127820e9d3a9075f
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-07-13 15:39:20 +0200

    fix agenix keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,7 @@
 let
-  personal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWXcUvknDXdGRkIRkOEZ153V5/JLEevroZjYiUqA1SY pthr@nixOS-server";
+  pthr-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWXcUvknDXdGRkIRkOEZ153V5/JLEevroZjYiUqA1SY pthr@nixOS-server";
   
-  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANUjzYfAisOLTJlyr8fM263TsnnDtUY+jjBcC53zen8 root@nixOS-server";
-  
-  # List of keys that can decrypt secrets
-  keys = [ personal server ];
+  keys = [ pthr-key ];
 in
 {
   "username1.age".publicKeys = keys;
