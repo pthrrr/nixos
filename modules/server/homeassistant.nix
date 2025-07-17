d6cb213ebc0143cb22c85cee6782aa474754f34a
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-07-17 08:23:37 +0200

    update

--- modules/server/homeassistant.nix
+++ modules/server/homeassistant.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
+
+let
+  domain = builtins.readFile "/run/agenix/domain";
+in
 {
   services.home-assistant = {
     enable = true;
@@ -8,15 +12,11 @@
     ];
     config = {
       default_config = {};
-      
-      # Top-level URL configuration (not under http)
-      external_url = "https://ha.my.domain";  # Replace with your actual domain
+      external_url = "https://ha.${builtins.replaceStrings ["\n"] [""] domain}";
       internal_url = "http://localhost:8123";
-      
-      # HTTP configuration for reverse proxy
       http = {
         trusted_proxies = [
-          "192.168.10.100"  # Your server's internal IP
+          "192.168.10.100"
           "127.0.0.1"
           "::1"
         ];
