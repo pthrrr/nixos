0b57d394ebfe93a5341f689706f1dc914ddacffd
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-11-21 09:13:02 +0100

    update freshrss

--- modules/server/podman/freshrss.nix
+++ modules/server/podman/freshrss.nix
@@ -2,25 +2,6 @@
 let
   stripNL = s: builtins.replaceStrings ["\n"] [""] s;
   domain = builtins.replaceStrings ["\n"] [""] (builtins.readFile config.age.secrets.domain.path);
-  
-  # Define your RSS feeds in OPML format
-  feedsOpml = pkgs.writeText "feeds.opml" ''
-    <?xml version="1.0" encoding="UTF-8"?>
-    <opml version="2.0">
-      <head>
-        <title>My RSS Feeds</title>
-      </head>
-      <body>
-        <outline text="Tech News" title="Tech News">
-          <outline type="rss" text="Hacker News" title="Hacker News" xmlUrl="https://news.ycombinator.com/rss" htmlUrl="https://news.ycombinator.com/"/>
-          <outline type="rss" text="Ars Technica" title="Ars Technica" xmlUrl="https://feeds.arstechnica.com/arstechnica/index" htmlUrl="https://arstechnica.com/"/>
-        </outline>
-        <outline text="NixOS" title="NixOS">
-          <outline type="rss" text="NixOS Discourse" title="NixOS Discourse" xmlUrl="https://discourse.nixos.org/posts.rss" htmlUrl="https://discourse.nixos.org/"/>
-        </outline>
-      </body>
-    </opml>
-  '';
 in
 {
   age.secrets.domain = {
@@ -39,15 +20,13 @@ in
         ];
         
         environment = {
-          CRON_MIN = "*/15";
+          CRON_MIN = "*/15";  # Update feeds every 15 minutes
           TZ = "Europe/Berlin";
         };
         
         volumes = [
           "freshrss-data:/var/www/FreshRSS/data"
           "freshrss-extensions:/var/www/FreshRSS/extensions"
-          # Mount the OPML file
-          "${feedsOpml}:/var/www/FreshRSS/data/feeds.opml:ro"
         ];
         
         extraOptions = [
