821786bc37dbf01a1bf042d4626488e0fcea448a
pthrrr <29637548+pthrrr@users.noreply.github.com>
2025-11-21 09:01:29 +0100

    add freshrss, move podman services to /modules/server/podman

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -147,6 +147,17 @@ in
         reverse_proxy localhost:8384
       }
 
+      rss.$DOMAIN {
+        tls {
+          dns namecheap {
+            user {env.NAMECHEAP_API_USER}
+            api_key {env.NAMECHEAP_API_KEY}
+            client_ip $SERVER_IP
+          }
+        }
+        reverse_proxy localhost:8082
+      }
+
       shelly1.$DOMAIN {
         tls {
           dns namecheap {
