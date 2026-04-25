d15572c94f9a60616fe6eca6fbd8b85dfc37d23e
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-04-25 22:53:05 +0200

    add ai subdomain

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -221,13 +221,6 @@ in
       }
       
       ai.$DOMAIN {
-        tls {
-          dns namecheap {
-            user {env.NAMECHEAP_API_USER}
-            api_key {env.NAMECHEAP_API_KEY}
-            client_ip $SERVER_IP
-          }
-        }
         reverse_proxy localhost:8282
       }
 
