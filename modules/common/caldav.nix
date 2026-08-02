75a824358ad46a3b58a7cfd0a4f3ccf9d0eecc4c
pthrrr <29637548+pthrrr@users.noreply.github.com>
2026-08-02 23:48:34 +0200

    fix caldav: read age secrets directly in home module

--- modules/common/caldav.nix
+++ modules/common/caldav.nix
@@ -1,13 +1,6 @@
 { config, lib, pkgs, ... }:
 let
-  stripNL = s: builtins.replaceStrings ["\n"] [""] s;
   cfg = config.services.caldav;
-  credentials = pkgs.writeText "caldav-credentials" ''
-    {
-      "domain" = "${stripNL cfg.domain}";
-      "username" = "${stripNL cfg.username}";
-      "password" = "${stripNL cfg.password}";
-    };
 in
 {
   options.services.caldav = {
@@ -26,12 +19,6 @@ in
     password = lib.mkOption {
       default = "";
       description = "Radical user password";
-    };
-  };
-
-  config = lib.mkIf config.services.caldav.enable {
-    home-manager.users.pthr.file = {
-      ".config/caldav/credentials.json".source = credentials;
     };
   };
 }
